55f23adc3201dc574926acfac8b3bf2583e22149
Sanshrit <sanshritbakshi@gmail.com>
2025-07-05 20:12:23 +0530

    Add modules

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -20,7 +20,7 @@ export default function Modules() {
             </button>
             <ul id="wd-modules">
                 <li className="wd-module">
-                    <div className="wd-title">Week 1</div>
+                    <div className="wd-title">Week 1 -  Course Introduction, Syllabus, Agenda</div>
                     <ul className="wd-lessons">
                         <li className="wd-lesson">
                             <span className="wd-title">LEARNING OBJECTIVES</span>
@@ -30,12 +30,87 @@ export default function Modules() {
                             </ul>
                         </li>
                     </ul>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">READING</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">Full Stack Developer - Chapter 1 - Introduction</li>
+                                <li className="wd-content-item">Full Stack Developer - Chapter 2 - Creating User</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">SLIDES</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">Introduction to Web Development</li>
+                                <li className="wd-content-item">Creating an HTTP server with Node.js</li>
+                                <li className="wd-content-item">Creating a React Application</li>
+                            </ul>
+                        </li>
+                    </ul>
                 </li>
                 <li className="wd-module">
-                    <div className="wd-title">Week 2</div>
+                    <div className="wd-title">Week 2 - HTML Fundamentals and Structure</div>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">LEARNING OBJECTIVES</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">Understand HTML document structure</li>
+                                <li className="wd-content-item">Learn basic HTML elements and attributes</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">READING</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">Full Stack Developer - Chapter 3 - HTML Basics</li>
+                                <li className="wd-content-item">Full Stack Developer - Chapter 4 - HTML Elements</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">SLIDES</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">HTML Document Structure</li>
+                                <li className="wd-content-item">Common HTML Elements</li>
+                                <li className="wd-content-item">HTML Attributes and Properties</li>
+                            </ul>
+                        </li>
+                    </ul>
                 </li>
                 <li className="wd-module">
-                    <div className="wd-title">Week 3</div>
+                    <div className="wd-title">Week 3 - CSS Styling and Layout</div>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">LEARNING OBJECTIVES</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">Introduction to CSS and styling concepts</li>
+                                <li className="wd-content-item">Learn CSS selectors and properties</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">READING</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">Full Stack Developer - Chapter 5 - CSS Fundamentals</li>
+                                <li className="wd-content-item">Full Stack Developer - Chapter 6 - CSS Layout</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul className="wd-lessons">
+                        <li className="wd-lesson">
+                            <span className="wd-title">SLIDES</span>
+                            <ul className="wd-content">
+                                <li className="wd-content-item">Introduction to CSS</li>
+                                <li className="wd-content-item">CSS Selectors and Specificity</li>
+                                <li className="wd-content-item">Box Model and Layout Basics</li>
+                            </ul>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </div>
