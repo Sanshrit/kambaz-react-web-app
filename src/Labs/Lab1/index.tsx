4e1453a150e9b466bd8bda6d3e318cfeb3dd9b74
Sanshrit <sanshritbakshi@gmail.com>
2025-07-08 21:45:52 +0530

    A1 Changes

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -162,7 +162,7 @@ export default function Lab1() {
                     <label htmlFor="wd-text-fields-username">Username:</label>
                     <input placeholder="jdoe" id="wd-text-fields-username" /> <br />
                     <label htmlFor="wd-text-fields-password">Password:</label>
-                    <input type="password" value="123@#$asd" id="wd-text-fields-password" />
+                    <input type="password" defaultValue="123@#$asd" id="wd-text-fields-password" />
                     <br />
                     <label htmlFor="wd-text-fields-first-name">First name:</label>
                     <input type="text" title="John" id="wd-text-fields-first-name" /> <br />
