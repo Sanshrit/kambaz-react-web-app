73f5717b57f628a86867b36963b2d7faae5768b5
Sanshrit <sanshritbakshi@gmail.com>
2025-07-15 20:10:21 +0530

    Assignment Link Changes

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -5,6 +5,7 @@ import AssignmentControlButtons from "./AssignmentControlButtons";
 import { FaClipboardList } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
 import AssignmentHandler from "./AssignmentHandler";
+import { Link } from "react-router-dom";
 
 export default function Assignments() {
     return (
@@ -18,8 +19,7 @@ export default function Assignments() {
                         ASSIGNMENTS
                         <AssignmentControlButtons />
                     </div>
-            
-                    {/* Single ListGroup container for all assignments with proper spacing */}
+
                     <ListGroup className="wd-lessons rounded-0">
                         <ListGroup.Item className="wd-lesson p-4 ps-2 d-flex align-items-start">
                             <div className="d-flex align-items-center">
@@ -27,9 +27,7 @@ export default function Assignments() {
                                 <FaClipboardList className="me-1 fs-3 text-success" />
                             </div>
                             <div className="ms-5 flex-grow-1">
-                                <a href="#/Kambaz/Courses/1234/Assignments/123" className="wd-assignment-link text-black fw-bold link-underline link-underline-opacity-0">
-                                    A1 - ENV + HTML
-                                </a>
+                                <Link to="/Kambaz/Courses/1234/Assignments/123" className="wd-assignment-link text-black fw-bold link-underline link-underline-opacity-0"> A1 - ENV + HTML</Link>
                                 <p className="mb-0">
                                     <span className="text-danger">Multiple Modules </span>| <b>Not Available until</b> May 13 at 12:00am | <b>Due</b> May 20 at 11:59pm | 100 pts
                                 </p>
@@ -38,16 +36,16 @@ export default function Assignments() {
                                 <AssignmentHandler />
                             </div>
                         </ListGroup.Item>
-                        
+
                         <ListGroup.Item className="wd-lesson p-4 ps-2 d-flex align-items-start">
                             <div className="d-flex align-items-center">
                                 <BsGripVertical className="fs-3" />
                                 <FaClipboardList className="me-1 fs-3 text-success" />
                             </div>
                             <div className="ms-5 flex-grow-1">
-                                <a href="#/Kambaz/Courses/1234/Assignments/124" className="wd-assignment-link text-black fw-bold link-underline link-underline-opacity-0">
+                                <Link to="/Kambaz/Courses/1234/Assignments/124" className="wd-assignment-link text-black fw-bold link-underline link-underline-opacity-0">
                                     A2 - CSS + BOOTSTRAP
-                                </a>
+                                </Link>
                                 <p className="mb-0">
                                     <span className="text-danger">Multiple Modules </span>| <b>Not Available until</b> May 20 at 12:00am | <b>Due</b> May 27 at 11:59pm | 100 pts
                                 </p>
@@ -56,16 +54,16 @@ export default function Assignments() {
                                 <AssignmentHandler />
                             </div>
                         </ListGroup.Item>
-                        
+
                         <ListGroup.Item className="wd-lesson p-4 ps-2 d-flex align-items-start">
                             <div className="d-flex align-items-center">
                                 <BsGripVertical className="fs-3" />
                                 <FaClipboardList className="me-1 fs-3 text-success" />
                             </div>
                             <div className="ms-5 flex-grow-1">
-                                <a href="#/Kambaz/Courses/1234/Assignments/125" className="wd-assignment-link text-black fw-bold link-underline link-underline-opacity-0">
+                                <Link to="/Kambaz/Courses/1234/Assignments/125" className="wd-assignment-link text-black fw-bold link-underline link-underline-opacity-0">
                                     A3 - JAVASCRIPT + REACT
-                                </a>
+                                </Link>
                                 <p className="mb-0">
                                     <span className="text-danger">Multiple Modules </span>| <b>Not Available until</b> May 27 at 12:00am | <b>Due</b> June 3 at 11:59pm | 100 pts
                                 </p>
