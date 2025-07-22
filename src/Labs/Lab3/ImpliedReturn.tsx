afdd663aa3dd4958e8bb16141b42ecb59cfdfc5c
Sanshrit <sanshritbakshi@gmail.com>
2025-07-23 00:13:07 +0530

    Formatting changes

--- src/Labs/Lab3/ImpliedReturn.tsx
+++ src/Labs/Lab3/ImpliedReturn.tsx
@@ -1,8 +1,8 @@
-const multiply = (a: number, b: number) => a * b;
-const fourTimesFive = multiply(4, 5);
-console.log(fourTimesFive);
-
 export default function ImpliedReturn() {
+    const multiply = (a: number, b: number) => a * b;
+const fourTimesFive = multiply(4, 5);
+    console.log(fourTimesFive);
+
     return (
         <div id="wd-implied-return">
             <h4>Implied return</h4>
