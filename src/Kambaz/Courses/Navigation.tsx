1ba9312f47a5a0f4f95165c020a413fc91b356e2
Sanshrit <sanshritbakshi@gmail.com>
2025-07-22 12:55:54 +0530

    external links, images and A3 work

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -2,7 +2,17 @@ import "../styles.css";
 import { Link } from "react-router-dom";
 import { useLocation, useParams } from "react-router";
 export default function CourseNavigation() {
-  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+  const links = ["Home", "Modules", "Assignments", "Quizzes", "Grades", "People"];
+  const extLinks = [
+    {
+      label: "Piazza",
+      url: "https://piazza.com/class/mchnh9kzzbu35p"
+    },
+    {
+      label: "Zoom",
+      url: "https://zoom.us/myhome"
+    }
+  ]
   const { cid } = useParams();
   const { pathname } = useLocation();
   return (
@@ -11,6 +21,10 @@ export default function CourseNavigation() {
         <Link to={`/Kambaz/Courses/${cid}/${link}`} className={
           `list-group-item border border-0 ${pathname.includes(link) ? "active" : "text-danger"}`}
         >{link}</Link>))}
+      {extLinks.map((link) => (
+        <a href={`${link.url}`} target="_blank"
+          className="list-group-item text-danger border border-0">{link.label}</a>
+      ))}
     </div>
   );
 }
--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -15,7 +15,7 @@ export default function Dashboard() {
                                 <Link to={`/Kambaz/Courses/${course._id}/Home`} className="wd-dashboard-course-link text-decoration-none text-dark">
                                 <Card.Img src={`/images/${course.image}`} variant="top" width="100%" height={160} />
                                 <Card.Body className="card-body">
-                                    <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                                    <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden text-truncate">
                                         {course.name} </Card.Title>
                                     <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
                                         {course.description} </Card.Text>
