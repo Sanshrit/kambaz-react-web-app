49fa8f167194d72108f693666d70988aa4e467f9
Sanshrit <sanshritbakshi@gmail.com>
2025-07-30 03:33:16 +0530

    Lab 5

---
+++ src/Labs/Lab5/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from "react-bootstrap";
+
+export default function Lab5() {
+  return (
+    <Container>
+    <div id="wd-lab5">
+      <h2>Lab 5</h2>
+      <div className="list-group">
+        <a href="http://localhost:4000/lab5/welcome"          
+           className="list-group-item">
+           Welcome
+        </a>
+      </div><hr/>
+    </div>
+    </Container>
+  );
+}
