5f35f418c4c95b3e22a37c869bc72cc5d51a025f
Sanshrit <sanshritbakshi@gmail.com>
2025-08-05 20:23:25 +0530

    removing db references from client

--- src/Kambaz/Courses/Assignments/reducer.ts
+++ src/Kambaz/Courses/Assignments/reducer.ts
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { assignments } from "../../Database";
+// import { assignments } from "../../Database";
 
 const initialState = {
-    assignments: assignments,
+    assignments: [],
     assignment: { 
         _id: "",
         title: "",
