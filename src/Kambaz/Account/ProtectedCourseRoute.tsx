f4da589f171013003eca1631c81b590e2c3fd7cd
Sanshrit <sanshritbakshi@gmail.com>
2025-08-12 12:13:18 +0530

    updating protected route

--- src/Kambaz/Account/ProtectedCourseRoute.tsx
+++ src/Kambaz/Account/ProtectedCourseRoute.tsx
@@ -1,42 +1,10 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { Navigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { setEnrollments } from "../Courses/People/reducer";
-import * as enrollmentsClient from "../Courses/People/client";
 
 export default function ProtectedCourseRoute({ children }: { children: any }) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const { enrollments } = useSelector((state: any) => state.enrollmentsReducer);
   const { cid } = useParams();
-  const dispatch = useDispatch();
-  const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   const fetchEnrollments = async () => {
-  //     if (currentUser?._id) {
-  //       try {
-  //         // Only fetch if enrollments array is empty
-  //         if (enrollments.length === 0) {
-  //           const serverEnrollments = await enrollmentsClient.fetchAllEnrollments();
-  //           dispatch(setEnrollments(serverEnrollments));
-  //         }
-  //       } catch (error) {
-  //         console.error("Error fetching enrollments:", error);
-  //       } finally {
-  //         setLoading(false);
-  //       }
-  //     } else {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchEnrollments();
-  // }, [currentUser, enrollments.length, dispatch]);
-
-  // Show loading while fetching enrollments
-  if (loading && enrollments.length === 0) {
-    return <div className="text-center p-4">Loading course access...</div>;
-  }
 
   if (!currentUser) {
     return <Navigate to="/Kambaz/Account/Signin" replace />;
